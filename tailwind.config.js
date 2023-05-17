bd2adeba01d13e94c892b25c84a1250954fbd5a0
Danielpark1239 <Danielpark1239@gmail.com>
2023-05-17 06:52:20 -0400

    Updated configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },
+    extend: {
+      colors: {
+        navy: '#0A2647',
+        lnavy: '#144272',
+        blue: '#205295',
+        lblue: '#2C74B3'
+
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    }
   },
   plugins: [],
 }
