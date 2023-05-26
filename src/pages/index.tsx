4ede1d6e60fc8d878bbb776e3a909c77ce49321f
Daniel Park <Danielpark1239@gmail.com>
2023-05-26 04:41:43 -0400

    Added framer-motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
+import { useState, useEffect } from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { motion, useMotionTemplate } from "framer-motion"
 import About from "../components/about"
 import Projects from "../components/projects"
 import Experience from "../components/experience"
@@ -9,8 +11,33 @@ import Contact from "../components/contact"
 import Links from "../components/links"
 
 const IndexPage: React.FC<PageProps> = () => {
+  const [mouseX, setMouseX] = useState(0)
+  const [mouseY, setMouseY] = useState(0)
+  const [scrollY, setScrollY] = useState(0)
+  const handleMouseMove = (event: any) => {
+    const { pageX, pageY } = event
+    setMouseX(pageX)
+    setMouseY(pageY)
+    console.log(pageX, pageY)
+  }
+  useEffect(() => {
+    window.addEventListener('mousemove', e => handleMouseMove(e))
+  }, [])
+
   return (
-    <div className='bg-navy w-screen h-screen font-inter mx-auto shadow-lg' >
+    <div className='group/1 relative bg-navy w-screen font-inter mx-auto shadow-lg'>
+      <motion.div
+        className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover/1:opacity-100"
+        style={{
+          background: useMotionTemplate`
+            radial-gradient(
+              650px circle at ${mouseX}px ${mouseY}px,
+              rgba(14, 165, 233, 0.15),
+              transparent 80%
+            )
+          `,
+        }} 
+      />
       <Navbar />
       <Links />
       <section id="about">
@@ -38,5 +65,6 @@ export const Head: HeadFC = () => (
   <>
     <title>Home Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <script src="https://kit.fontawesome.com/4d1a165625.js" crossOrigin="anonymous"></script>
   </>
 )
